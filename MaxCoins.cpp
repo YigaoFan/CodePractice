8b333b3af5b300db12abfe3162a56650b171456e
Yigao Fan <v-yifan@microsoft.com>
2020-05-26 19:53:51 +0800

    add MaxCoins.cpp

---
+++ MaxCoins.cpp
@@ -0,0 +1,21 @@
+// https://leetcode-cn.com/problems/burst-balloons/
+template <typename T>
+class Matrix
+{
+private:
+    T* _data;
+public:
+    Matrix(unsigned rowCount, unsigned colCount)
+        : _data(new T[rowCount * colCount]) // 应该用 allocate
+    { }
+
+    
+};
+class Solution 
+{
+public:
+    int maxCoins(vector<int>& nums) 
+    {
+        
+    }
+};
