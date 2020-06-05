8ccc422494a50ccbd6467ee77524ad7ffc37790e
YigaoFan <fanyigao@qq.com>
2020-06-05 17:29:20 +0800

    add FairCandySwap.cpp

---
+++ FairCandySwap.cpp
@@ -0,0 +1,86 @@
+class Enumerator
+{
+private:
+    vector<int> const& _vec;
+    size_t _i = 0;
+    bool _firstMove = true;
+public:
+    Enumerator(vector<int> const& vec) : _vec(vec)
+    { }
+
+    int current()
+    {
+        return _vec[_i];
+    }
+
+    bool moveNext()
+    {
+        if (_firstMove)
+        {
+            _firstMove = false;
+            return true;
+        }
+
+        if ((++_i) < _vec.size())
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+
+class Solution
+{
+public:
+    vector<int> fairCandySwap(vector<int>& A, vector<int>& B)
+    {
+        sort(A.begin(), A.end());
+        sort(B.begin(), B.end());
+
+        auto sumA = sumItemIn(A);
+        auto sumB = sumItemIn(B);
+        auto aver = (sumA + sumB) / 2;
+
+        auto dis = sumA - aver;
+        auto eA = Enumerator(A);
+        auto eB = Enumerator(B);
+
+        eA.moveNext();
+        eB.moveNext();
+        while (true)
+        {
+            auto itemA = eA.current();
+            auto itemB = eB.current();
+
+            if (auto r = itemA - dis; r > itemB)
+            {
+                eB.moveNext();
+            }
+            else if (r < itemB)
+            {
+                eA.moveNext();
+            }
+            else
+            {
+                return 
+                {
+                    itemA,
+                    itemB,
+                };
+            }
+        }
+    }
+
+    static int sumItemIn(vector<int> const& v)
+    {
+        auto sum = 0;
+
+        for (auto i = 0; i < v.size(); ++i)
+        {
+            sum += v[i];
+        }
+
+        return sum;
+    }
+};
