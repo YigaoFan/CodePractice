7eb70b249b27a6ff9543103a39499f69577b4d7a
YigaoFan <fanyigao@qq.com>
2020-06-12 22:57:18 +0800

    update RottingOranges.cpp

--- RottingOranges.cpp
+++ RottingOranges.cpp
@@ -5,72 +5,9 @@ public:
     int orangesRotting(vector<vector<int>>& grid)
     {
         auto total = 0;
-        auto freshOneNum = 0;
-        map<pair<int, int>, int> rottenPath;
-
-        // 感染后的橘子才有扩散能力，位置上有橘子才有扩散能力
-        function<void(int, int, int)> spread = [&rottenPath, &grid, &spread](int i, int j, int step)
-        {
-            // set up self
-            auto pos = pair<int, int>(i, j);
-            if (rottenPath.find(pos) == rottenPath.end())
-            {
-                rottenPath.insert({ { i, j }, step });
-            }
-            else if (rottenPath[pos] > step)
-            {
-                rottenPath[pos] = step;
-            }
-            else
-            {
-                return;
-            }
-            
-            // set up nearby
-            auto next = step + 1;
-            auto onTop = false;
-            auto onBottom = false;
-            if (i == 0)
-            {
-                onTop = true;
-            }
-            else if (i == grid.size() - 1)
-            {
-                onBottom = true;
-            }
-
-            if (!onTop)
-            {
-                spread(i - 1, j, next);
-            }
-
-            if (!onBottom)
-            {
-                spread(i + 1, j, next);
-            }
-
-            auto onLeft = false;
-            auto onRight = false;
-            if (j == 0)
-            {
-                onLeft = true;
-            }
-            else if (j == grid[0].size() - 1)
-            {
-                onRight = true;
-            }
-
-            if (!onLeft)
-            {
-                spread(i, j - 1, next);
-            }
-
-            if (!onRight)
-            {
-                spread(i, j + 1, next);
-            }
-        };
-
+        set<pair<int, int>> freshCoords;
+        set<pair<int, int>> rottedCoords;
+ 
         for (auto i = 0; i < grid.size(); ++i)
         {
             for (auto j = 0; j < grid[0].size(); ++j)
@@ -80,49 +17,114 @@ public:
 
                 if (item == 1)
                 {
-                    if (rottenPath.find(pos) == rottenPath.end())
-                    {
-                        rottenPath.insert({ { i, j }, INT_MAX });
-                    }
-                    else
-                    {
-                        spread(i, j, rottenPath[pos]);
-                    }
-                    
-                    ++freshOneNum;
+                    freshCoords.insert(pos);
                     ++total;
                 }
                 else if (item == 2)
                 {
-                    if (rottenPath.find(pos) == rottenPath.end())
-                    {
-                        rottenPath.insert({ { i, j }, INT_MAX });
-                    }
-                    else
-                    {
-                        rottenPath[pos] = 0;
-                        spread(i, j, rottenPath[pos]);
-                    }
-
-                    spread(i, j, 0);
+                    rottedCoords.insert(pos);
                     ++total;
                 }
             }
         }
 
-        if (total == 0 || freshOneNum == 0)
+        if (total == 0 || freshCoords.empty())
         {
             return 0;
         }
         else
         {
-            auto maxStep = 0;
-            for (auto& p : rottenPath)
+            auto getNearbyCoords = [&grid](set<pair<int, int>> const& coords)
             {
-                maxStep = max(maxStep, p.second);
-            }
+                set<pair<int, int>> nearbyCoords;
+                
+                for (auto& c : coords)
+                {
+                    auto i = c.first;
+                    auto j = c.second;
 
-            return maxStep == INT_MAX ? -1 : maxStep;
+                    auto onTop = false;
+                    auto onBottom = false;
+                    if (i == 0)
+                    {
+                        onTop = true;
+                    }
+                    if (i == (grid.size() - 1))
+                    {
+                        onBottom = true;
+                    }
+
+                    if (!onTop)
+                    {
+                        nearbyCoords.insert({ i - 1, j });
+                    }
+                    if (!onBottom)
+                    {
+                        nearbyCoords.insert({ i + 1, j });
+                    }
+
+                    auto onLeft = false;
+                    auto onRight = false;
+                    if (j == 0)
+                    {
+                        onLeft = true;
+                    }
+                    if (j == (grid[0].size() - 1))
+                    {
+                        onRight = true;
+                    }
+
+                    if (!onLeft)
+                    {
+                        nearbyCoords.insert({ i, j - 1 });
+                    }
+                    if (!onRight)
+                    {
+                        nearbyCoords.insert({ i, j + 1 });
+                    }
+                }
+
+                return nearbyCoords;
+            };
+
+            auto newRotted = move(rottedCoords);
+            for (auto min = 1; ; ++min)
+            {
+                newRotted = getNearbyCoords(newRotted);
+                removeRotted(freshCoords, newRotted);
+
+                if (freshCoords.empty())
+                {
+                    return min;
+                }
+
+                if (newRotted.empty())
+                {
+                    return -1;
+                }
+            }
+        }
+    }
+
+    static void removeRotted(set<pair<int, int>>& freshCoords, set<pair<int, int>>& newPossibleRotted)
+    {
+        vector<pair<int, int>> toDel;
+
+        for (auto& c : newPossibleRotted)
+        {
+            if (freshCoords.find(c) != freshCoords.end())
+            {
+                freshCoords.erase(c);
+            }
+            else
+            {
+                toDel.push_back(c);
+            }
+        }
+
+        for (auto& c : toDel)
+        {
+            newPossibleRotted.erase(c);
         }
     }
 };
